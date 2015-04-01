4a3d80a2a305a49b1a072e6afe7fc74c522b29ab
Alex Dellow <alex_dellow@hotmail.co.uk>
2015-04-01 13:14:19 -0700

    Actually checks passwords now

--- application/controllers/Auth.php
+++ application/controllers/Auth.php
@@ -13,7 +13,7 @@ class Auth extends Application {
 		$key = $_POST['userid'];
 		
 		$user = $this->users->get($key);
-		if (password_verify) {
+		if (password_verify($this->input->post('password'), $user->password)) {
 			$this->session->set_userdata('userID',$key);
 			$this->session->set_userdata('userName',$user->name);
 			$this->session->set_userdata('userRole',$user->role);
